4dddef78cb035ee480a60226708fee0a26166dfd
friedmanroi <rfilinb@gmail.com>
2023-08-22 18:07:07 +0300

    first commit

--- test/Buyme.py
+++ test/Buyme.py
@@ -38,6 +38,73 @@ class Test_end_to_end:
         assert current_url == expected_url, "this is the url"
         pick_it.stage3()
         time.sleep(5)
-        result_element = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[value='17573218']")))
-        result_element.click()
+
+    def test_select_gift(self):
+        self.login.login(constants.EMAIL, constants.PASSWORD)
+        time.sleep(10)
+        selection_page = SelectionPage(self.driver, self.wait)
+        selection_page.select_sum()
+        selection_page.select_category()
+        selection_page.select_region()
+        selection_page.submit_page()
         self.wait.until(EC.element_to_be_clickable((By.ID, "ember1199"))).click()
+        time.sleep(5)
+
+        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".bm-subtitle-1")))[0].click()
+
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[placeholder='הכנס סכום']"))).send_keys("50")
+
+        # Wait for the presence of all elements with type "submit" and click the first one
+        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[type=submit]")))[0].click()
+
+        time.sleep(5)
+
+        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".bm-subtitle-1")))[0].click()
+
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[placeholder='הכנס סכום']"))).send_keys("50")
+
+        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[type=submit]")))[0].click()
+
+        time.sleep(5)
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-parsley-required-message='מי הזוכה המאושר? יש להשלים את שם המקבל/ת']"))).send_keys("zohar")
+
+# Wait for the element with title "לאיזה אירוע?" to be visible and click it
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[title='לאיזה אירוע?']"))).click()
+
+        time.sleep(5)
+
+# Wait for the presence of all elements with value "10" and click the second one (index 1)
+        self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[value='10']")))[1].click()
+
+# Wait for the element with data-parsley-group "voucher-greeting" to be visible and clear it, then send keys "mazal tov"
+        greeting_element = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-parsley-group='voucher-greeting']")))
+        greeting_element.clear()
+        greeting_element.send_keys("mazal tov")
+
+        # Upload a file
+        self.driver.find_element(By.CSS_SELECTOR, "input[name=logo]").send_keys("test/dog.jpg")
+
+        # Wait for the element with type "submit" to be visible and click it
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[type=submit]"))).click()
+
+        # Wait for the element with gtm "עכשיו" to be visible and click it
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[gtm='עכשיו']"))).click()
+
+        # Wait for the element with gtm "method-sms" to be visible and click it
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[gtm='method-sms']"))).click()
+
+        # Wait for the element with placeholder "נייד מקבל/ת המתנה" to be visible, clear it, and send keys "0542281191"
+        mobile_element = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[placeholder='נייד מקבל/ת המתנה']")))
+        mobile_element.clear()
+        mobile_element.send_keys("0542281191")
+
+        # Wait for the element with placeholder "שם שולח המתנה" to be visible, clear it, and send keys "moran"
+        sender_element = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[placeholder='שם שולח המתנה']")))
+        sender_element.clear()
+        sender_element.send_keys("roi")
+
+        # Wait for the element with placeholder "מספר נייד" to be visible, clear it, and send keys "0542281191"
+        mobile_number_element = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[placeholder='מספר נייד']")))
+        mobile_number_element.clear()
+        mobile_number_element.send_keys("0542281191")
+        time.sleep(5)
